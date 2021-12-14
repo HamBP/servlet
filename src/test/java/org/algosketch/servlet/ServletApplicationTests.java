24bcd65692d156dc7cb3e3819d04177aadd65897
algosketch <sjy_9484@naver.com>
2021-12-14 22:15:35 +0900

    init

---
+++ src/test/java/org/algosketch/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package org.algosketch.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
